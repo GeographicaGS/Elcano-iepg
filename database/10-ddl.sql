82a883296fe24a4e6a5b5e4f58b4e3129590b424
Juan Pedro Perez <jp.alcantara@gstgis.com>
2014-02-03 18:54:01 +0100

    Initial database structure settled.

--- database/10-ddl.sql
+++ database/10-ddl.sql
@@ -13,6 +13,7 @@ begin;
 create schema iepg_data authorization :user;
 
 create table iepg_data.master_countries(
+  id varchar(10),
   cod_unstats_num char(3),
   cod_unstats_alpha char(3),
   cod_eurostat char(2),  
@@ -23,7 +24,11 @@ create table iepg_data.master_countries(
 
 alter table iepg_data.master_countries
 add constraint master_countries_pkey
-primary key(cod_unstats_num);
+primary key(id);
+
+alter table iepg_data.master_countries
+add constraint master_countries_unique_cod_unstats_num
+unique(cod_unstats_num);
 
 alter table iepg_data.master_countries
 add constraint master_countries_unique_cod_unstats_alpha
@@ -43,23 +48,23 @@ unique(name_es);
 
 
 create table iepg_data.country_relation(
-  cod_unstats_child char(3),
-  cod_unstats_parent char(3)
+  id_child varchar(10),
+  id_parent varchar(10)
 );
 
 alter table iepg_data.country_relation
 add constraint country_relation_pkey
-primary key(cod_unstats_child, cod_unstats_parent);
+primary key(id_child, id_parent);
 
 alter table iepg_data.country_relation
 add constraint country_relation_master_countries_child_fkey
-foreign key (cod_unstats_child)
-references iepg_data.master_countries(cod_unstats_num);
+foreign key (id_child)
+references iepg_data.master_countries(id);
 
 alter table iepg_data.country_relation
 add constraint country_relation_master_countries_parent_fkey
-foreign key (cod_unstats_parent)
-references iepg_data.master_countries(cod_unstats_num);
+foreign key (id_parent)
+references iepg_data.master_countries(id);
 
 
 -- Copy country data
@@ -77,6 +82,23 @@ with delimiter ';'
 csv header quote '"';
 
 \c :dbname :user
+
+
+create table iepg_data.metadata_variable(
+  id_variable char(32),
+  name_en varchar(250),
+  name_es varchar(250),
+  description_en text,
+  description_es text,
+  discrete boolean,  
+  code_type varchar(25),
+  data_type varchar(25)
+);
+
+alter table iepg_data.metadata_variable
+add constraint metadata_variable_pkey
+primary key (id_variable);
+  
 
 commit;
 

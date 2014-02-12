501aa01ee7c87d2a99080e350b63c6767e9d46f7
Juan Pedro Perez <jp.alcantara@gstgis.com>
2014-02-12 13:57:39 +0100

    Advances in PostgreSQL model.

--- www-srv/src/backend/helperloadfile.py
+++ www-srv/src/backend/helperloadfile.py
@@ -4,8 +4,11 @@
 import iepgdataimport.core as core
 reload(core)
 
-l = core.FileLoader('backend/Energy_2.csv', '\r', 'mac_roman')
+# l = core.FileLoader('backend/Energy_2.csv', '\r', 'mac_roman')
 
-c = core.ImportIepgData(l.readFile())
-c.processData()
+# c = core.ImportIepgData(l.readFile())
+# c.processData()
+
+m = core.ImportModel()
+m.createTable('iepg_data', 'test_table', ['field1', 'field2'], ['integer', 'varchar(10)'])
 
--- www-srv/src/backend/iepgdataimport/core.py
+++ www-srv/src/backend/iepgdataimport/core.py
@@ -50,11 +50,29 @@ class FileLoader(object):
 class ImportModel(PostgreSQLModel):
     """Model for importing IEPG CSV into the PostgreSQL."""
     __insertData = []
+    __insertDataSchema = []
 
     def __init__(self):
         """Constructor."""
         PostgreSQLModel.__init__(self)
         self.__insertData = []
+        self.__insertDataSchema = []
+
+    def createTable(self, tableSchema, tableName, fieldName, fieldType):
+        """
+        Creates a table with a given schema. The 'field*' parameter are
+        lists containing the schema of the table obviously they have
+        to match in dimensions.
+        """
+        sql = 'create table '+tableSchema+'.'+tableName+'('
+
+        for i in range(0, len(fieldName)):
+            print(fieldName[i])
+            sql += fieldName[i]+' '+fieldType[i]+','
+
+        sql = sql.rstrip(',')+');'
+
+        print(sql)
 
     def insertMetadata(self, id_variable, variable_en, variable_es, description_en, description_es, \
                        info_type, iepg_type, code_type, variable_type, data_type):
@@ -98,6 +116,9 @@ class ImportModel(PostgreSQLModel):
         sql = 'delete from iepg_data.'+variable_en+';'
         self.queryCommit(sql)
 
+    def setInsertDataSchema(self, schema):
+        self.__insertDataSchema = schema
+
     def clearInsertData(self):
         """Clears the insertion data buffer."""
         self.__insertData = []
@@ -110,10 +131,19 @@ class ImportModel(PostgreSQLModel):
         """Makes the inserts."""
         sql = ''
 
+        print(self.__insertData[0])
+
         for row in self.__insertData:
             print(row)
-            sql += 'insert into iepg_data.'+table+' ' \
-                   'values(
+            sql = 'insert into iepg_data.'+table+'('
+
+            for field in row:
+                sql += field+','
+
+            sql = sql.rstrip(',')+') values('
+
+            for a in range(0,len(self.__insertDataSchema)):
+                sql += row # +self.__insertDataSchema[a]
 
         print sql
 
@@ -292,6 +322,7 @@ class ImportIepgData(object):
 
                     self.__conn.addToInsertData(d)
 
+        self.__conn.setInsertDataSchema(['varchar(10)', 'integer', 'value'])
         self.__conn.flushInsertData(meta['variable_en'])
 
     def __getMetadataDictionary(self, meta):

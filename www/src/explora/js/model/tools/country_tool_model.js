8f19b80117eb83e020545fe90df32213f9ee3629
Alberto <alberto.asuero@geographica.gs>
2014-04-25 17:27:33 +0200

    Sync

--- www/src/explora/js/model/tools/country_tool_model.js
+++ www/src/explora/js/model/tools/country_tool_model.js
@@ -1,14 +1,14 @@
 app.model.tools.country = Backbone.Model.extend({
 
     url: function(){
-        return this.urlRoot() + "/" + this.get("id") + "/" + this.get("year") +  "/" + this.get("variable") + "/" +  app.lang;
+        return this.urlRoot() + "/" + app.lang +  "/" + this.get("id") + "?filter=ES,US";
     },
 
     urlRoot: function() {
-        return app.config.API_URL + "/country";
+        return app.config.API_URL + "/countrysheet";
     },
     
     parse : function(response,options){
-        return response;
+        return response.results;
     }
 });

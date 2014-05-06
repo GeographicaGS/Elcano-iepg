1b69b12673c0d4123e610ed73f8dbb22054554a0
Juan Pedro Perez <jp.alcantara@geographica.gs>
2014-05-06 10:40:51 +0200

    Commited config_changes.py

--- www-srv/src/common/config_changes.py
+++ www-srv/src/common/config_changes.py
@@ -3,7 +3,10 @@
 """
 Configuration
 """
-backend = {'tmpFolder': '/home/git/elcano-iepg/www/cdn/backend/tmp', \
-           'mediaFolder': '/home/git/elcano-iepg/www/cdn/media'}
+backend = {'tmpFolder': '/home/git/elcano-iepg/www/cdn/backend/tmp',
+           'mediaFolder': '/home/git/elcano-iepg/www/cdn/media',
+           'DocumentListLength': 10,
+           'UnpublishedHighlightCatalogBackendListLength': 10
+}
 
 MemcachedConfig = {'enabled': False, 'host': 'localhost', 'port': '11211', 'expiration': 30}

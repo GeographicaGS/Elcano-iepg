37da973fa01258dfd47236c467ca801fb2d0bac3
Alberto <alasarr@MacBook-Pro-de-Alberto.local>
2014-05-12 15:09:19 +0200

    sync

--- www/src/explora/js/view/tools/common/slider_singlepoint_view.js
+++ www/src/explora/js/view/tools/common/slider_singlepoint_view.js
@@ -16,6 +16,41 @@ app.view.tools.common.SliderSinglePoint = app.view.tools.common.Slider.extend({
         }));
 
         this.$el.show();
+
+        this.$ul = this.$("ul.slider");
+
+        var _this = this;
+
+        this.$(".draggable").draggable({ 
+            axis: "x" , 
+            containment: "parent",
+            stop: function( event, ui ) {
+                var ulwidth = _this.$ul.width(),
+                    pos = ui.position.left,
+                    perc_pos = (100* pos) / ulwidth,
+                    mindist = ulwidth;
+
+                // let's find the closest element 
+                var $closest = null;
+
+                _this.$ul.children("[base]").each(function(idx, el){
+
+                    var dist = Math.abs($(el).position().left -pos);
+                    if (dist < mindist){
+                        mindist = dist;
+                        $closest = $(el);
+                    }
+                });
+
+                // let's move the element to the closest
+                $(ui.helper).find(".ornament").html($closest.find(".ornament").html() );
+                $(ui.helper).animate({
+                    left: $closest.position().left
+                }, 200,function(){
+                    $closest.find("a.point").trigger("click");
+                });
+            }
+        });
         
     },
 

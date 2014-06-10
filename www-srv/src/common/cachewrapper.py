fdbea831575e7db35750779fa0d879cdc9924e84
Juan Pedro Perez <jp.alcantara@geographica.gs>
2014-06-10 18:41:32 +0200

    Fixes to cachewrapper, several imports missing.

--- www-srv/src/common/cachewrapper.py
+++ www-srv/src/common/cachewrapper.py
@@ -6,6 +6,9 @@ Cache wrapper.
 
 """
 import common.config as config
+import hashlib
+import memcache
+
 
 def cacheWrapper(funcName, *args, **kwargs):
     """Cache wrapping helper."""
@@ -16,13 +19,13 @@ def cacheWrapper(funcName, *args, **kwargs):
         for i in kwargs:
             s+=str(i)
         h = hashlib.sha256(funcName.__name__+s).hexdigest()
-        mc = memcache.Client([MemcachedConfig["host"]+":"+MemcachedConfig["port"]], debug=0)
+        mc = memcache.Client([config.MemcachedConfig["host"]+":"+config.MemcachedConfig["port"]], debug=0)
         v = mc.get(h)
         if v:
             return(v)
         else:
             out = funcName(*args, **kwargs)
-            mc.set(h, out, MemcachedConfig["expiration"])
+            mc.set(h, out, config.MemcachedConfig["expiration"])
             return(out)
     else:
         return(funcName(*args, **kwargs))

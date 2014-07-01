fbf31bfc51dfdc061c44e26175a933a72ff1381d
Raúl Yeguas <raul.yeguas@geographica.gs>
2014-07-01 15:37:25 +0200

    Selected country bars pagination improve

--- www/src/explora/js/view/tools/common/countries_view.js
+++ www/src/explora/js/view/tools/common/countries_view.js
@@ -7,7 +7,11 @@ app.view.tools.common.Countries = Backbone.View.extend({
     initialize: function(options){
         this._variableCtrlStatus = options && options.variable!=undefined && options.variable!="undefined" ? options.variable : true; 
         this._draggable = options && options.draggable!=undefined && options.draggable!="undefined" ? options.draggable : false; 
-       
+        
+        var self = this;
+        $(window).on('resize', function(){
+            self.render();
+        });
     },
 
     _events: {

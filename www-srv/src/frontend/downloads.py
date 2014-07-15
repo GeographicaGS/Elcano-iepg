89fa70db6be71740788a255c6f73cf032f60f9f4
Juan Pedro Perez <jp.alcantara@geographica.gs>
2014-07-15 14:01:08 +0200

    WIP

--- www-srv/src/frontend/downloads.py
+++ www-srv/src/frontend/downloads.py
@@ -105,7 +105,6 @@ def getDownloadData(language, years, variables, countries, columns, rows):
             allVariables = dict(allVariables.items()+{varFamily+"@"+k: v for (k,v)
                                                       in dc.dataSets[varFamily].variables.iteritems()}.items())
 
-
         allVariables = OrderedDict(sorted(allVariables.items(), key=lambda t: 
                                           const.variableNames[t[0].split("@")[0]][t[0].split("@")[1]]["order"]))
 
@@ -114,32 +113,34 @@ def getDownloadData(language, years, variables, countries, columns, rows):
         print "tabData : ", tabData
 
         col = 1
-        worksheet = workbook.add_worksheet(('Datos año '+str(year)).decode("utf-8"))
-        title = "Data from Elcano Global Presedfdfdfdfnce Index (year "+str(year)+")" if language=="en" else \
-                "Datos del Índice de Presencia Globaldfdfdfdf Elcano (año "+str(year)+")"
+        worksheet = workbook.add_worksheet((str(tabData)).decode("utf-8"))
+        title = "Data from Elcano Global Presence Index ("+str(tabData)+")" if language=="en" else \
+                "Datos del Índice de Presencia Globaldfdfdfdf Elcano ("+str(tabData)+")"
         worksheet.set_row(0,30,title_format)
         worksheet.write(0,0,title.decode("utf-8"))
-        for k,v in allVariables.iteritems():
-            data = sorted(chelpers.getData(v, countryList=countries, year=year).values(),
-                          key=lambda t: translate[t["code"]])
-            family,name = k.split("@")
-            worksheet.set_row(1,22,header_format)
-            worksheet.write(1,col,family.upper()+": "+
-                            const.variableNames[family][name]["name_"+language].decode("utf-8"), header_format)
-            row = 2
-            for d in data:
-                if (row-1)%3==0:
-                    worksheet.set_row(row,15,highlight)
-                else:
-                    worksheet.set_row(row,15,no_highlight)
-                worksheet.write(row, 0, translate[d["code"]].decode("utf-8"))
-                worksheet.set_column(0, 0, 25)
-                worksheet.write(row, col, d["value"])
-                worksheet.set_column(col, col, 15)
-                row+=1
-            col+=1
+        # for k,v in allVariables.iteritems():
+        #     data = sorted(chelpers.getData(v, countryList=countries, year=year).values(),
+        #                   key=lambda t: translate[t["code"]])
+        #     family,name = k.split("@")
+        #     worksheet.set_row(1,22,header_format)
+        #     worksheet.write(1,col,family.upper()+": "+
+        #                     const.variableNames[family][name]["name_"+language].decode("utf-8"), header_format)
+        #     row = 2
+        #     for d in data:
+        #         if (row-1)%3==0:
+        #             worksheet.set_row(row,15,highlight)
+        #         else:
+        #             worksheet.set_row(row,15,no_highlight)
+        #         worksheet.write(row, 0, translate[d["code"]].decode("utf-8"))
+        #         worksheet.set_column(0, 0, 25)
+        #         worksheet.write(row, col, d["value"])
+        #         worksheet.set_column(col, col, 15)
+        #         row+=1
+        #     col+=1
 
-        worksheet.write(row+1, 0, "Más inforkdkdkdkmación:".decode("utf-8") if language=="es" else 
+        row = 10
+
+        worksheet.write(row+1, 0, "Más información:".decode("utf-8") if language=="es" else 
                         "More information:".decode("utf-8"), bold_format)
         worksheet.write_url(row+1, 1, "http://www.globalpresence.realinstitutoelcano.org", url_format,
                             "http://www.globalpresence.realinstitutoelcano.org")

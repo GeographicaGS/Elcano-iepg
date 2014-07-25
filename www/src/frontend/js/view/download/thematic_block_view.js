1eefed68a3809cd9f452eaec4e388b989428c139
Alberto Asuero <alberto.asuero@geographica.gs>
2014-07-25 17:22:53 +0200

    Remove militar from downloads IEPE

--- www/src/frontend/js/view/download/thematic_block_view.js
+++ www/src/frontend/js/view/download/thematic_block_view.js
@@ -2,20 +2,14 @@ app.view.thematicBlock = Backbone.View.extend({
     _template : _.template( $('#thematic_block_template').html() ),
     
     initialize: function() {
-        
-//        this.render();
 
-        
     },
     
     events:{
     	"click .tematica": "tematicaClick",
     	"click .tematica2": "tematicaClick",
     	"click .todos": "todosClick",
-    	
     },
-    
-
 
     onClose: function(){
         // Remove events on close
@@ -28,17 +22,28 @@ app.view.thematicBlock = Backbone.View.extend({
         this.$el.html(this._template());
         
         var self = this;
-        
-        this.$el.find(".container").append(this.getHtmlThematic("iepg","iepe", "iepg","iepe", ""));
-        
-        $(app.view.tools.utils.variablesTree().data.children).each(function() {
-        	self.$el.find(".container").append(self.getHtmlThematic(this.name,this.name , this.key, this.key, "level2"));
-        	$(this.children).each(function() {
-        		self.$el.find(".container").append(self.getHtmlThematic(this.name,this.name, this.key, this.key, "level3"));
-        	});
-        });
-        
+
+        self.renderFamily("iepg");
+        self.renderFamily("iepe");
+
         return this;
+    },
+
+    renderFamily: function(f){
+        var html = "",
+            self = this;
+
+        _.each(app.view.tools.utils.variablesTree().data.children,function(d){
+            if (f=="iepe" && d.key=="military_global") return;
+
+            html += self.getHtmlThematic(d.name,d.key, "level2");
+
+            _.each(d.children,function(c) {
+                html += self.getHtmlThematic(c.name, c.key, "level3");
+            });
+        });
+
+        this.$("#cod_variables_"+f).html(html);
     },
     
     tematicaClick: function(e) {
@@ -92,15 +97,18 @@ app.view.thematicBlock = Backbone.View.extend({
     		if($(e.currentTarget).hasClass("tematica")){
     			$(".tematica").removeClass("active");
                 $(".tematica").siblings("div").removeClass("iconVariableActive");
-    		}else{
+    		}
+            else{
     			$(".tematica2").removeClass("active");
                 $(".tematica2").siblings("div").removeClass("iconVariableActive");
     		}
-    	}else{
+    	}
+        else{
     		if($(e.currentTarget).hasClass("tematica")){
     			$(".tematica").addClass("active");
                 $(".tematica").siblings("div").addClass("iconVariableActive");
-    		}else{
+    		}
+            else{
     			$(".tematica2").addClass("active");
                 $(".tematica2").siblings("div").addClass("iconVariableActive");
     		}
@@ -110,35 +118,36 @@ app.view.thematicBlock = Backbone.View.extend({
 
         if(total == 1){
             $(".numBloqsSelect2").html(sprintf("<lang> %d variable seleccionada</lang>",total));
-        }else{
+        }
+        else{
             $(".numBloqsSelect2").html(sprintf("<lang> %d variables seleccionadas</lang>",total));
         }
 
     	if($(".counter.numAniosSelect").text() == "0" || $(".counter.numPaises").text() == "0" || $(".counter.numBloqsSelect").text() == "0"){
     		$(".boxDonwload").removeClass("activeDownload");
-    	}else{
+    	}
+        else{
     		$(".boxDonwload").addClass("activeDownload");
     	}
     	
     	if($(".counter.numBloqsSelect").text() == "0"){
     		$(".counter.numBloqsSelect").siblings("img").attr("src","/img/ELC_flecha_descarga_paso.svg")
-    	}else{
+    	}
+        else{
     		$(".counter.numBloqsSelect").siblings("img").attr("src","/img/ELC_flecha_descarga_paso-selec.svg")
     	}
     	
     },
     
-    getHtmlThematic: function(name1, name2, key1, key2, level) {
-    	return "<div class='row " + level + "'>" +
+    getHtmlThematic: function(name,key, level) {
+    	var html = "<div class='row " + level + "'>" +
 					"<div class='col-sm-6 col-md-6'>"+
-						"<div class='iconVariable' data-variable='" + name1 + "'></div>"+
-						"<div class='tematica' key='" + key1 + "'>" + app.variableToString(name1) + "</div>"+
-					"</div>"+
-					"<div class='col-sm-6 col-md-6' style='padding-left: " + (level == "level2" ? '30' : (level == "level3" ? '50':'10')) + "px;'>"+
-						"<div class='iconVariable pl ml' data-variable='" + name2 + "'></div>"+
-						"<div class='tematica2' key='" + key2 + "'>" + app.variableToString(name2) + "</div>"+
-					"</div>"+
-				"</div>";
+						"<div class='iconVariable' data-variable='" + name + "'></div>"+
+						"<div class='tematica' key='" + key + "'>" + app.variableToString(name) + "</div>"+
+					"</div>";
+        html += "</div>";
+
+        return html;
     	
     },
 

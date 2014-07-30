8dad12bb86601cd75280b6cb0dc870033861ee6a
Juan Pedro Perez <jp.alcantara@geographica.gs>
2014-07-30 19:36:57 +0200

    TimeInterval WIP.

--- iepg-data-calculation-engine/data/core.py
+++ iepg-data-calculation-engine/data/core.py
@@ -15,6 +15,68 @@ y = np.empty((2,2))
 # x[:,:,0] = np.random.rand(2,2)
 
 
+
+
+class TimeInterval(object):
+    """Time interval for Equidna."""
+    start = None
+    end = None
+
+    def __init__(self, startStr, endStr):
+        """Initializator."""
+        self.start = datetime.strptime(startStr, self.__getStrptime(startStr)) \
+                     if startStr else None
+        self.end = datetime.strptime(endStr, self.__getStrptime(endStr)) \
+                   if endStr else None
+
+        if self.start and self.end and self.start>=self.end:
+            self.start = None
+            self.end = None
+
+    def timeIn(self, time):
+        if isinstance(time, str):
+            time = datetime.strptime(time, self.__getStrptime(time))
+
+        if self.start and self.end and self.start<=time and time<=self.end:
+            return(True)
+        if self.start and self.end is None and self.start<=time:
+            return(True)
+        if self.start is None and self.end and time<=self.end:
+            return(True)
+
+        return(False)
+
+    def __str__(self):
+        """To str."""
+        return("TimeInterval: "+str(self.start)+" | "+str(self.end))
+
+    def __getStrptime(self, strptime):
+        """Get the strptime."""
+        if ":" in strptime:
+            strp = "%Y-%m-%d %H:%M"
+        else:
+            strp = "%Y-%m-%d"
+
+        return(strp)
+
+        
+
+
+    
+
+
+
+e0 = TimeInterval("2013-01-01", "2014-01-01")
+e1 = TimeInterval("2015-01-01", "2014-01-01")
+e2 = TimeInterval("2013-01-01", None)
+e3 = TimeInterval(None, "2015-01-01")
+e4 = TimeInterval("2014-01-01", "2015-01-01")
+t = datetime(2013,6,1)
+print e0.timeIn(t), e1.timeIn(t), e2.timeIn(t), e3.timeIn(t), e4.timeIn(t)
+
+
+
+
 class GeoVariableArray(object):
     """Data array for Equidna."""
     data = None
@@ -28,8 +90,6 @@ class GeoVariableArray(object):
         self.__time = time
         self.__variable = []
         self.data = np.empty((len(self.__geoentity), len(self.__time), 1))
-        # self.data = np.linspace(0, 100, num=len(self.__geoentity)*len(self.__time)*2). \
-        #             reshape((len(self.__geoentity), len(self.__time), 2))
 
     def size(self):
         """Returns dimensions."""
@@ -37,6 +97,19 @@ class GeoVariableArray(object):
 
     def __getitem__(self, key):
         print key
+        print type(key)
+        print type(key[0])
+        print type(key[1])
+        print type(key[2])
+        if isinstance(key[0], str):
+            geo = [key[0]]
+        if isinstance(key[1], str):
+            time = [key[1]]
+        if isinstance(key[2], str):
+            var = [key[2]]
+
+        print geo, time, var
+        
         return(self.data[key])
 
     def addGeoentities(self, geoentities):
@@ -53,25 +126,13 @@ class GeoVariableArray(object):
     def addVariable(self, array, name):
         self.__variable.append(name)
         array.resize(len(self.__geoentity), len(self.__time), 1)
-
         if len(self.__variable)==1:
             self.data = array
-
         else: 
             self.data = np.append(self.data, array, axis=2)
-
-        print(self.__variable)
-
-
         self.data.resize((len(self.__geoentity), len(self.__time), len(self.__variable)))
-        # self.data[:,:,len(self.__variable)-1] = array
 
-
-
-
-        # else:
-        #     self.data = np.append(self.data, array, axis=2)
-
+    
 
 
 

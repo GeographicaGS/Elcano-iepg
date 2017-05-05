4844ed5b6775f5b0f596acd2e4e770ed2f61a309
cayetanobv <cayetano.benavent@geographica.gs>
2017-05-05 12:02:09 +0200

    improved CLI to update from calculated

--- www-srv/src/flux_updatefromcalculatedxlsx.py
+++ www-srv/src/flux_updatefromcalculatedxlsx.py
@@ -10,18 +10,36 @@ flux_updatefromcalculatedxlsx.py calculus2015.xlxs [# of years in IEPG] [# of co
 
 """
 
+import argparse
 import common.flux as flux
-import sys
 
-fileName = sys.argv[1]
-nIepgYears = int(sys.argv[2])
-nIepgCountries = int(sys.argv[3])
-nIepeYears = int(sys.argv[4])
-nIepeCountries = int(sys.argv[5])
 
-print "Trying to import file %s with %s IEPG years, %s IEPG countries, %s IEPE years, and %s IEPE countries" % \
-  (fileName, nIepgYears, nIepgCountries, nIepeYears, nIepeCountries)
+def run():
+    
+    descr = "Loads a calculated XLSX into the database"
+    arg_parser = argparse.ArgumentParser(description=descr)
+    
+    arg_parser.add_argument('file_name', type=str, help='XLSX filename')
+    arg_parser.add_argument('n_iepg_years', type=int, help='# of years in IEPG')
+    arg_parser.add_argument('n_iepg_countries', type=int, help='# of countries in IEPG')
+    arg_parser.add_argument('n_iepe_years', type=int, help='# of years in IEPE')
+    arg_parser.add_argument('n_iepe_countries', type=int, help='# of countries in IEPE')
+    
+    args = arg_parser.parse_args()
 
-f = flux.Flux()
+    fileName = args.file_name
+    nIepgYears = args.n_iepg_years
+    nIepgCountries = args.n_iepg_countries
+    nIepeYears = args.n_iepe_years
+    nIepeCountries = args.n_iepe_countries
 
-o = f.loadCalculatedXlsxToDatabase(fileName, nIepgYears, nIepgCountries, nIepeYears, nIepeCountries)
+    print("Trying to import file %s with %s IEPG years, %s IEPG countries, %s IEPE years and %s IEPE countries" % \
+      (fileName, nIepgYears, nIepgCountries, nIepeYears, nIepeCountries))
+
+    f = flux.Flux()
+
+    o = f.loadCalculatedXlsxToDatabase(fileName, nIepgYears, nIepgCountries, nIepeYears, nIepeCountries)
+
+
+if __name__ == '__main__':
+    run()

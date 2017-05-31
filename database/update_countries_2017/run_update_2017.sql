10df0f7bda21c96dffbd8121d7c183f022e76bf4
cayetanobv <cayetano.benavent@geographica.gs>
2017-05-31 13:43:57 +0200

    update translations

--- database/update_countries_2017/run_update_2017.sql
+++ database/update_countries_2017/run_update_2017.sql
@@ -5,4 +5,5 @@ BEGIN;
 \i database/update_countries_2017/update_blocks.sql
 \i database/update_countries_2017/update_congord_name.sql
 \i database/update_countries_2017/update_countrysheets.sql
+\i database/update_countries_2017/update_www_translation.sql
 COMMIT;

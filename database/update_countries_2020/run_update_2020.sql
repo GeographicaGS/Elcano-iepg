c3eae8b4d6f86c895bef886858aded041274721b
cayetanobv <cayetano.benavent@geographica.gs>
2020-07-14 13:07:21 +0200

    update 2020 main script and package.json updated

---
+++ database/update_countries_2020/run_update_2020.sql
@@ -0,0 +1,8 @@
+--
+-- TRANSACTION TO UPDATE countries - 09/05/2018
+--
+BEGIN;
+\ir database/update_countries_2018/update_blocks.sql
+-- \ir database/update_countries_2018/cupdate_countrysheets.sql
+-- \ir database/update_countries_2018/update_www_translation.sql
+COMMIT;

3a1cc0b3f4cb3292914c7afd230092435c99eb7c
cayetanobv <cayetano.benavent@geographica.gs>
2021-07-14 11:22:23 +0200

    fixed database scripts

--- database/update_countries_2021/update_blocks.sql
+++ database/update_countries_2021/update_blocks.sql
@@ -13,7 +13,7 @@
 --  'Niger',
 --  'Moldova',
 --  'Chad',
---  'Congo DR')
+--  'Congo R')
 -- AND gn.id_name_family=2;
 -- --
 -- -- Get blocks:
@@ -162,19 +162,19 @@ BEGIN;
       )
     );
 
-  -- INSERT INTO
-  --   maplex.block (id_geoentity_block,id_geoentity_child)
-  --   VALUES (
-  --     (
-  --       select gn.id_geoentity as id_geoentity_xbap from maplex.geoentity_name gn
-  --       INNER JOIN maplex.name n ON gn.id_name=n.id_name
-  --       where n.name='Sub-Saharan Africa' and gn.id_name_family=2
-  --     ),
-  --     (
-  --       select gn.id_geoentity as id_geoentity_xbap from maplex.geoentity_name gn
-  --       INNER JOIN maplex.name n ON gn.id_name=n.id_name
-  --       where n.name = 'Congo DR' and gn.id_name_family=2
-  --     )
-  --   );
+  INSERT INTO
+    maplex.block (id_geoentity_block,id_geoentity_child)
+    VALUES (
+      (
+        select gn.id_geoentity as id_geoentity_xbap from maplex.geoentity_name gn
+        INNER JOIN maplex.name n ON gn.id_name=n.id_name
+        where n.name='Sub-Saharan Africa' and gn.id_name_family=2
+      ),
+      (
+        select gn.id_geoentity as id_geoentity_xbap from maplex.geoentity_name gn
+        INNER JOIN maplex.name n ON gn.id_name=n.id_name
+        where n.name = 'Congo' and gn.id_name_family=2
+      )
+    );
 
 COMMIT;

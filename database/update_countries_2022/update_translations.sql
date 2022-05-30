7b245a5bdc474bfe2b96d977c0be945033cf1872
Sergio Clebal <sergioclebal@gmail.com>
2022-05-30 11:17:42 +0200

    Actualizar footer

--- database/update_countries_2022/update_translations.sql
+++ database/update_countries_2022/update_translations.sql
@@ -838,7 +838,7 @@ UPDATE www.translation SET es = 'El Índice Elcano de Presencia Global cubre una
 UPDATE www.translation SET en = 'The Elcano Global Presence Index includes 150 countries. These are selected, mainly, according to their size in terms of GDP' WHERE key = '_faq text 12';
 
 
-UPDATE www.translation SET es = '2014 - 2021 &copy; Real Instituto Elcano' WHERE key = 'Copyright TEXT';
-UPDATE www.translation SET en = '2014 - 2021 &copy; Elcano Royal Institute' WHERE key = 'Copyright TEXT';
+UPDATE www.translation SET es = '2014 - 2022 &copy; Real Instituto Elcano' WHERE key = 'Copyright TEXT';
+UPDATE www.translation SET en = '2014 - 2022 &copy; Elcano Royal Institute' WHERE key = 'Copyright TEXT';
 
 COMMIT;

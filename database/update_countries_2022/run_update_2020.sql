af21ee6a9e2d621bfd1569262244a6a7bf470a95
Sergio Clebal <sergioclebal@gmail.com>
2022-05-30 11:33:13 +0200

    Update run_update_2020

--- database/update_countries_2022/run_update_2020.sql
+++ database/update_countries_2022/run_update_2020.sql
@@ -2,8 +2,8 @@
 -- -- TRANSACTION TO UPDATE countries
 -- --
 BEGIN;
-  \ir database/update_countries_2021/update_blocks.sql
-  \ir database/update_countries_2021/pop_gdp.sql
-  \ir database/update_countries_2021/update_countrysheets.sql
-  \ir database/update_countries_2021/update_translations.sql
+  \ir database/update_countries_2022/update_blocks.sql
+  \ir database/update_countries_2022/pop_gdp.sql
+  \ir database/update_countries_2022/update_countrysheets.sql
+  \ir database/update_countries_2022/update_translations.sql
 COMMIT;
